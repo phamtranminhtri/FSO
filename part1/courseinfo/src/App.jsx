4ff3f9a16667cd94aa503cccd59c9925ebcacb07
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-04-17 22:33:09 +0700

    1.1 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,31 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part}: {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises: {props.num_exercises}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = "Half stack app development"
   const part1 = "Fundamental of React"
@@ -9,19 +37,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-      <p>
-        Number of exercises: {exercise1 + exercise2 + exercise3}
-      </p>
+      <Header course={course} />
+      <Content part={part1} exercise={exercise1} />
+      <Content part={part2} exercise={exercise2} />
+      <Content part={part3} exercise={exercise3} />
+      <Total num_exercises={exercise1 + exercise2 + exercise3} />
     </div>
   )
 }
