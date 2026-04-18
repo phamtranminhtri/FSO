cdf7961994c420372667387d15dde8500516f2c1
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-04-18 11:03:14 +0700

    event handle 2

--- part1/practice/part_1c/src/App.jsx
+++ part1/practice/part_1c/src/App.jsx
@@ -3,19 +3,18 @@ import { useState } from "react"
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  const handleClick = () => {
-    console.log("clicked")
-  }
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
   console.log("rendering...", counter)
 
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={increaseByOne}>
         plus
       </button>
-      <button onClick={() => setCounter(0)}>
+      <button onClick={setToZero}>
         zero
       </button>
     </div>
