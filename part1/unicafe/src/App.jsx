554c325de6e5cb3b69bd3a73ef57d1904cebcc73
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-04-18 19:49:50 +0700

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -17,6 +17,10 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -28,6 +32,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
