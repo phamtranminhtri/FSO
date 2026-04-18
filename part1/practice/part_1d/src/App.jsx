269827b92d5612079e263589a77f7eb506833d7f
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-04-18 14:26:34 +0700

    array

--- part1/practice/part_1d/src/App.jsx
+++ part1/practice/part_1d/src/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from "react"
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
-  const handleRightClicks = () => setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClicks}>
-        right
-      </button>
-      {clicks.right}
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
