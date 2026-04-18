c4621ce90e8a0dfa4c7158c7c1cff4c7594598cb
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-04-18 19:33:54 +0700

    1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,3 +1,35 @@
-const App = () => {}
+import { useState } from "react"
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const addBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={addGood}>good</button>
+      <button onClick={addNeutral}>neutral</button>
+      <button onClick={addBad}>bad</button>
+
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
 
 export default App
