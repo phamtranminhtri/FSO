32e9e10a5beca71efee49e0f6e3f25156283ec47
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-04-18 14:15:36 +0700

    1 object

--- part1/practice/part_1d/src/App.jsx
+++ part1/practice/part_1d/src/App.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react"
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClicks = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
+      {clicks.left}
+      <button onClick={handleLeftClick}>
         left
       </button>
-      <button onClick={() => setRight(right + 1)}>
+      <button onClick={handleRightClicks}>
         right
       </button>
-      {right}
+      {clicks.right}
     </div>
   )
 }
