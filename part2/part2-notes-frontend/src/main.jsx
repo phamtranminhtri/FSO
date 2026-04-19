c179704eef995924df044068bb13608f25d99684
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-04-19 22:22:20 +0700

    effect hook

--- part2/part2-notes-frontend/src/main.jsx
+++ part2/part2-notes-frontend/src/main.jsx
@@ -1,45 +1,4 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import axios from "axios"
 
-const promise = axios.get("http://localhost:3001/notes")
-promise.then(response => {
-  console.log(response)
-})
-
-// console.log(promise)
-
-// const promise2 = axios.get("http://localhost:3000/foobar")
-// console.log(promise2)
-
-
-axios
-  .get("http://localhost:3001/notes")
-  .then(response => {
-    const notes = response.data
-    console.log(notes)
-    ReactDOM.createRoot(document.getElementById("root")).render(<App notes={notes} />)
-  })
-
-
-// const notes = [
-//   {
-//     id: 1,
-//     content: 'HTML is easy',
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: 'Browser can execute only JavaScript',
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     important: true,
-//   },
-// ]
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <App notes={notes} />
-// )
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
